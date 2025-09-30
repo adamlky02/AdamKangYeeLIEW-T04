c10d554ff0e061ecc84a591bd5b7b2c11bacf63f
adamliewky <adamliewky@gmail.com>
2025-09-30 08:19:35 +0800

    usage

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const currentPage = window.location.pathname.split("/").pop();
+    const navLinks = document.querySelectorAll(".navbar a");
+
+    navLinks.forEach(link => {
+        if (link.getAttribute("href") === currentPage) {
+            link.classList.add("active");
+        }
+    });
+});
+
+
