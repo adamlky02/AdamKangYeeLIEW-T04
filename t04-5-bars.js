38b928bb5959d5fe8ee395d5f894f9796cd9ae80
adamlky02 <adamliewky@gmail.com>
2025-10-07 00:15:15 +0800

    t04-5

---
+++ t04-5-bars.js
@@ -0,0 +1,11 @@
+// t04-5-bars.js
+const createBarChart = (data) => {
+    const svg = d3.select(".responsive-svg-container")
+        .append("svg")
+        .attr("viewBox", "0 0 1200 400") // temporary; we’ll adjust layout soon
+        .style("border", "1px solid black"); // dev-only border so we see the canvas
+    svg
+        .selectAll("rect")
+        .data(data)
+        .join("rect");
+};
