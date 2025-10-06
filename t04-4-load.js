47f1973146a292d06bfc44234ad0b7dc78e8b2d5
adamlky02 <adamliewky@gmail.com>
2025-10-07 00:52:48 +0800

    t04-5feat(t04-5): bind data to rects; set width/height; defer spacing to T04-6

--- t04-4-load.js
+++ t04-4-load.js
@@ -28,8 +28,8 @@ d3.csv("data/tvBrandCount.csv", d => ({
     console.log("max:", d3.max(data, d => d.count));
     console.log("min:", d3.min(data, d => d.count));
     console.log("extent:", d3.extent(data, d => d.count)); // [min, max]
-//Optional: sort for easier reading (descending by count)
+//sort for  (descending by count)
     data.sort((a, b) => d3.descending(a.count, b.count));
-// Hand off to the chart builder (implemented next exercise)
+// chart builder (implemented next exercise)
     createBarChart(data);
 });
