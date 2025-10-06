11f829703b80902a8236305e8c7cdc7dd30afa03
adamlky02 <adamliewky@gmail.com>
2025-10-07 00:54:33 +0800

    t04-5feat(t04-5): bind data to rects; set width/height; defer spacing to T04-6

--- t04-5-bars.js
+++ t04-5-bars.js
@@ -5,17 +5,12 @@ const createBarChart = (data) => {
         .attr("viewBox", "0 0 1200 400")
         .style("border", "1px solid black");
 
-    const barHeight = 16;
-    const barSpacing = 4; // gap between bars
-
     svg.selectAll("rect")
         .data(data)
         .join("rect")
-        .attr("class", d => `bar bar-${d.count}`)
-        .attr("width", d => d.count)
-        .attr("height", barHeight)
-        .attr("x", 0) // start at left
+            .attr("class", d => `bar bar-${d.count}`)
+            .attr("width", d => d.count)
+            .attr("height", 16)
         .attr("y", (d, i) => i * (barHeight + barSpacing)) // offset each bar
-        .attr("fill", "steelblue"); // optional color;
 };
 
